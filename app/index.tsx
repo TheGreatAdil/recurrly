2dde81aac0500027a20d6692842f74c00e007af8
Adil Anwar <adildoesart@gmail.com>
2026-03-30 16:06:05 +0530

    Installed NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="h-full items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
